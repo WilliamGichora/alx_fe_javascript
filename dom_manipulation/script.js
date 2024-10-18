f49b5b9574ff885552b30c94e9dac494acd36972
Mark Gichora <markgichora7@gmail.com>
2024-10-18 11:50:56 +0300

    Added Functionality to dynamically add random quotes

---
+++ dom_manipulation/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const quoteContainer = document.getElementById('quoteDisplay')
+    const showQuoteBtn = document.getElementById('newQuote')
+
+    const newQuoteInput = document.getElementById('newQuoteText')
+    const newQuoteCategory = document.getElementById('newQuoteCategory')
+    const addNewQuoteBtn = document.getElementById('addNewQuoteBtn')
+
+    const quotes = [
+        {
+            text: "Whoever is happy will make others happy.",
+            category: "Happiness"
+        },
+        {
+            text: "Everything you have ever wanted is sitting on the other side of fear.",
+            category: "Motivational"
+        },
+        {
+            text: "Everything you have ever wanted is sitting on the other side of fear.",
+            category: "Motivational"
+        }]
+
+    showQuoteBtn.addEventListener('click', displayRandomQuote)
+    
+    addNewQuoteBtn.addEventListener('click', addQuote)
+
+    function addQuote() {
+        const inputText = newQuoteInput.value.trim()
+        const newCategoryText = newQuoteCategory.value.trim()
+
+        if (inputText === '' || newCategoryText === '') {
+            alert("Please enter both a quote and a category.");
+            return;
+        }
+
+        const newQuote = {
+            text: inputText,
+            category: newCategoryText
+        }
+
+        quotes.push(newQuote)
+        newQuoteInput.value = '';
+        newQuoteCategory.value = '';
+
+    }
+
+    function displayRandomQuote() {
+        const index = Math.floor(Math.random() * quotes.length)
+        console.log(index);
+
+        const quoteParagraph = quotes[index]
+        quoteContainer.textContent = quoteParagraph.text
+    }
+
+})
